3abd06e9ea7eee9431a631deb532730adc59c763
horzwxy <wangxiayang.fdt@gmail.com>
2013-09-25 17:01:26 +0800

    complete nickname servlet

--- src/me/horzwxy/app/pfm/server/servlet/SetNicknameServlet.java
+++ src/me/horzwxy/app/pfm/server/servlet/SetNicknameServlet.java
@@ -7,7 +7,9 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import me.horzwxy.app.pfm.model.SetNicknameResponse;
 import me.horzwxy.app.pfm.model.User;
+import me.horzwxy.app.pfm.model.SetNicknameRequest;
 import me.horzwxy.app.pfm.server.model.UserDAO;
 
 public class SetNicknameServlet extends HttpServlet {
@@ -15,9 +17,11 @@ public class SetNicknameServlet extends HttpServlet {
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
-		String email = req.getParameter( "email" );
-		String nickname = req.getParameter( "nickname" );
+		String email = req.getParameter( SetNicknameRequest.EMAIL_KEY );
+		String nickname = req.getParameter( SetNicknameRequest.NICKNAME_KEY );
 		User user = new User( email, nickname );
 		UserDAO.updateUser( user );
+		
+		resp.getWriter().println( new SetNicknameResponse( SetNicknameResponse.SetNicknameResponseType.SUCCESS ).getPostContent() );
 	}
 }
